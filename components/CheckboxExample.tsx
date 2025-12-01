0beeddaee47ff107bf8c8c97f3b5ccffc64dc3de
wssf-wrj <WangRenJie777@outlook.com>
2025-12-01 11:21:37 +0800

    feat: add Checkbox callback

--- components/CheckboxExample.tsx
+++ components/CheckboxExample.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { ScrollView, Text, View, Alert } from "react-native";
 import { Checkbox, Icon } from "beeshell-ls";
 
@@ -29,118 +29,152 @@ export default function CheckboxExample() {
     const [value_5, setStateValue_5] = useState([])
     const [value_6, setStateValue_6] = useState([])
     const [value_7, setStateValue_7] = useState([])
+    const [callbackLogs, setCallbackLogs] = useState<any[]>([])
+    const scrollViewRef = useRef(null)
     return (
         <ScrollView>
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>基础</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>基础</Text>
+                <Checkbox
                     value={value_1}
                     onChange={(value: []) => {
                         setStateValue_1(value)
                     }}
-                    style={{padding: 20}}>
+                    style={{ padding: 12 }}>
                     {
-                        box_items.map(item => 
+                        box_items.map(item =>
                             <Checkbox.Item key={item.value} value={item.value} label={item.label} />
                         )
                     }
                 </Checkbox>
             </View>
 
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>选中状态改变回调</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>选中状态改变回调</Text>
+                <Text style={{ fontSize: 14, marginBottom: 4 }}>回调日志</Text>
+                <ScrollView ref={scrollViewRef} style={{ height: 100, padding: 12, backgroundColor: '#FFFFE0' }}>
+                    {
+                        callbackLogs.map(item => (
+                            <View style={{paddingBottom: 4}} key={Math.random()}>
+                                <Text>{item.time}</Text>
+                                <Text>{item.opt}</Text>
+                            </View>
+                        ))
+                    }
+                </ScrollView>
+                <Checkbox
                     value={value_7}
                     onChange={(value: []) => {
+                        // 获取当前时间的 Date 对象
+                        const currentDate = new Date();
+
+                        // 获取时间戳（毫秒）
+                        const timestamp = currentDate.getTime();
+
+                        // 获取各个时间组成部分
+                        const year = currentDate.getFullYear();
+                        const month = currentDate.getMonth() + 1; // 月份从0开始，需要+1
+                        const day = currentDate.getDate();
+                        const hours = currentDate.getHours();
+                        const minutes = currentDate.getMinutes();
+                        const seconds = currentDate.getSeconds();
+
                         setStateValue_7(value)
-                        Alert.alert('选中值', `当前选中值：${value}`)
+                        setCallbackLogs(callbackLogs.concat([
+                            {
+                                time: `${year}-${month}-${day} ${hours}:${minutes}:${seconds} onChange`,
+                                opt: `当前选中值：${value}`
+                            }
+                        ]))
+                        setTimeout(() => {
+                            scrollViewRef.current.scrollToEnd()
+                        })
                     }}
-                    style={{padding: 20}}>
+                    style={{ padding: 12 }}>
                     {
-                        box_items.map(item => 
+                        box_items.map(item =>
                             <Checkbox.Item key={item.value} value={item.value} label={item.label} />
                         )
                     }
                 </Checkbox>
             </View>
 
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>图标位置右</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>图标位置右</Text>
+                <Checkbox
                     value={value_2}
                     onChange={(value: []) => {
                         setStateValue_2(value)
                     }}
                     iconPosition="right"
-                    style={{padding: 20}}>
+                    style={{ padding: 12 }}>
                     {
-                        box_items.map(item => 
+                        box_items.map(item =>
                             <Checkbox.Item key={item.value} value={item.value} label={item.label} />
                         )
                     }
                 </Checkbox>
             </View>
 
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>可全选</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>可全选</Text>
+                <Checkbox
                     value={value_3}
                     onChange={(value: []) => {
                         setStateValue_3(value)
                     }}
                     showAllCheck={true}
-                    style={{padding: 20}}>
+                    style={{ padding: 12 }}>
                     {
-                        box_items.map(item => 
+                        box_items.map(item =>
                             <Checkbox.Item key={item.value} value={item.value} label={item.label} />
                         )
                     }
                 </Checkbox>
             </View>
 
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>自定义选中图标</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>自定义选中图标</Text>
+                <Checkbox
                     value={value_4}
                     onChange={(value: []) => {
                         setStateValue_4(value)
                     }}
                     checkedIcon={<Icon type='star' source={star} size={20} tintColor={variables.mtdBrandPrimaryDark} />}
-                    style={{padding: 20}}>
+                    style={{ padding: 12 }}>
                     {
-                        box_items.map(item => 
+                        box_items.map(item =>
                             <Checkbox.Item key={item.value} value={item.value} label={item.label} />
                         )
                     }
                 </Checkbox>
             </View>
 
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>自定义未选中图标</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>自定义未选中图标</Text>
+                <Checkbox
                     value={value_5}
                     onChange={(value: []) => {
                         setStateValue_5(value)
                     }}
-                    uncheckedIcon={<Icon type='star' source={star_o}  size={20} tintColor={variables.mtdGrayLightest} />}
-                    style={{padding: 20}}>
+                    uncheckedIcon={<Icon type='star' source={star_o} size={20} tintColor={variables.mtdGrayLightest} />}
+                    style={{ padding: 12 }}>
                     {
-                        box_items.map(item => 
+                        box_items.map(item =>
                             <Checkbox.Item key={item.value} value={item.value} label={item.label} />
                         )
                     }
                 </Checkbox>
             </View>
 
-            <View style={{ marginTop: 20, padding: 20 }}>
-                <Text style={{fontSize: 20, marginBottom: 12}}>自定义子选项</Text>
-                <Checkbox 
+            <View style={{ padding: 12 }}>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>自定义子选项</Text>
+                <Checkbox
                     value={value_6}
                     onChange={(value: []) => {
                         setStateValue_6(value)
-                        Alert.alert('选中值', `选中状态：${value}`)
                     }}
-                    style={{padding: 20}}>
+                    style={{ padding: 12 }}>
                     <Checkbox.Item value={1} label={'自定义选项label'} />
                     <Checkbox.Item value={2} label={'自定义选项disabled'} disabled={true} />
                     <Checkbox.Item value={7} label={'自定义选项renderItem'} renderItem={(checked) => (
@@ -148,7 +182,7 @@ export default function CheckboxExample() {
                             <Text>选中状态：</Text>
                             <Text>{checked ? 'true' : 'false'}</Text>
                         </View>
-                    ) } />
+                    )} />
                 </Checkbox>
             </View>
         </ScrollView>
