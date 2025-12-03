96f73086a5bcae457469d29bc68e901384c011a3
wssf-wrj <WangRenJie777@outlook.com>
2025-12-03 18:38:48 +0800

    feat: edit Progress demo
    Signed-off-by: wssf-wrj <WangRenJie777@outlook.com>

--- components/ProgressExample.tsx
+++ components/ProgressExample.tsx
@@ -12,28 +12,23 @@ export default function ProgressExample() {
     return (
         <ScrollView>
             <View style={{ padding: 12 }}>
-                <Text style={{ fontSize: 16, marginBottom: 4 }}>基础</Text>
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>基础 [percent值： {percentVal}]</Text>
                 <Progress percent={percentVal} />
             </View>
 
             <View style={{ padding: 12 }}>
-                <Text style={{ fontSize: 16, marginBottom: 4 }}>样式添加</Text>
-                <Progress style={{ height: 16, borderRadius: 8 }} barStyle={{ height: 16, borderRadius: 8 }} percent={percentVal} />
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>style和barStyle样式 [percent值： {percentVal}]</Text>
+                <Progress style={{ height: 16, borderRadius: 8, backgroundColor: '#21a5c3ff' }} barStyle={{ height: 16, borderRadius: 8, backgroundColor: '#0000CD' }} percent={percentVal} />
             </View>
 
             <View style={{ padding: 12 }}>
-                <Text style={{ fontSize: 16, marginBottom: 4 }}>动画持续2000ms</Text>
-                <Progress style={{ height: 16, borderRadius: 8 }} barStyle={{ height: 16, borderRadius: 8 }} percent={percentVal} duration={2000} />
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>有动画，动画持续2000ms [percent值： {percentVal}]</Text>
+                <Progress percent={percentVal} duration={2000} />
             </View>
 
             <View style={{ padding: 12 }}>
-                <Text style={{ fontSize: 16, marginBottom: 4 }}>没有动画</Text>
-                <Progress style={{ height: 16, borderRadius: 8 }} barStyle={{ height: 16, borderRadius: 8 }} percent={percentVal} easing={false} />
-            </View>
-
-            <View style={{ padding: 12 }}>
-                <Text style={{ fontSize: 16, marginBottom: 4 }}>设置进度条颜色</Text>
-                <Progress style={{ height: 16, borderRadius: 8 }} barStyle={{ height: 16, borderRadius: 8, backgroundColor: '#0000CD' }} percent={percentVal} duration={2000} />
+                <Text style={{ fontSize: 16, marginBottom: 4 }}>没有动画 [percent值： {percentVal}]</Text>
+                <Progress percent={percentVal} easing={false} />
             </View>
 
             <View style={{ padding: 12 }}>
